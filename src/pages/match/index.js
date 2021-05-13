faed889d4606ab8e07a509b65ca3c44dd64594e4
FreitasGa <freitasga.01@gmail.com>
2021-05-12 22:19:37 -0300

    match update

--- src/pages/match/index.js
+++ src/pages/match/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import { API } from "aws-amplify"
 
@@ -7,22 +7,13 @@ import { onError } from "../../libs/errorLib";
 import "./styles.css";
 
 export default function Match() {
-  const [params, setParams] = useState({
-    id: "",
-    match: ""
-  });
-  
-  const pathParams = useParams();
-
-  setParams({
-    id: pathParams.id,
-    match: pathParams.match,
-  });
+  const { id, match } = useParams();
+  console.log(id , match);
 
   useEffect(() => {
     async function onLoad() {
       try {
-        await API.put("cupido-online", `/message/${params.id}/match/${params.match}`);
+        await API.put("cupido-online", `/message/${id}/match/${match}`);
       } catch (err) {
         console.log(err);
         onError(err);
@@ -30,7 +21,7 @@ export default function Match() {
     }
 
     onLoad();
-  }, [params]);
+  });
 
   return (
     <div className="Match">
